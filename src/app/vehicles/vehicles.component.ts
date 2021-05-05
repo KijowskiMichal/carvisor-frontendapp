43a2b1298d2e9bfbf305d296284652562c1fb49b
xNieuczesana <weronka1999@gmail.com>
2021-05-05 14:59:38 +0200

    Finished API for Users and Vehicles, changed Edit Vehicle

--- src/app/vehicles/vehicles.component.ts
+++ src/app/vehicles/vehicles.component.ts
@@ -1,15 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-vehicles',
   templateUrl: './vehicles.component.html',
   styleUrls: ['./vehicles.component.scss']
 })
-export class VehiclesComponent implements OnInit {
 
-  constructor() { }
+export class VehiclesComponent implements OnInit {
+  listOfDevicesWrapper: ListOfDevicesWrapper;
+  page: number;
+  pageMax: number;
+  pageSize = 1;
+
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    this.http.get<LoginStatus>('/API/authorization/status').subscribe(value => {
+      if (!value.logged)
+      {
+        this.router.navigate(['./']);
+      }
+    });
+    this.list(1, '$');
   }
 
+  public list(page: number, regex: string): void
+  {
+    if (regex === '')
+    {
+      regex = '$';
+    }
+    if (page >= 1)
+    {
+      this.http.get<ListOfDevicesWrapper>('/API/devices/list/' + page + '/' + this.pageSize + '/' + regex + '/').subscribe(value => {
+        if (page <= value.pageMax)
+        {
+          this.listOfDevicesWrapper = value;
+          this.page = value.page;
+          this.pageMax = value.pageMax;
+        }
+      });
+    }
+  }
+
+}
+
+interface ListOfDevices {
+  image: string;
+  licensePlate: string;
+  distance: number;
+  model: string;
+  id: number;
+  brand: string;
+  status: string;
+}
+
+interface ListOfDevicesWrapper {
+  page: number;
+  listOfDevices: ListOfDevices[];
+  pageMax: number;
+}
+
+interface LoginStatus {
+  logged: boolean;
+  rbac: string;
+  nickname: string;
 }
