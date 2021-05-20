f589fd27ab6e10a808629e00775898fd912e0bb5
xNieuczesana <weronka1999@gmail.com>
2021-05-21 01:30:17 +0200

    Map points added

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -14,8 +14,8 @@ export class MapComponent implements OnInit {
 
   constructor(private http:HttpClient, private router:Router) { }
 
-  latitude: number = 52.460164;
-  longitude: number = 16.918042;
+  latitude: number = 52.460394146699365;
+  longitude: number = 16.917809968543395;
 
   ngOnInit() {
     this.map = new ol.Map({
@@ -30,16 +30,34 @@ export class MapComponent implements OnInit {
         zoom: 18
       })
     });
+
     var layer = new ol.layer.Vector({
       source: new ol.source.Vector({
         features: [
           new ol.Feature({
             geometry: new ol.geom.Point(ol.proj.fromLonLat([this.longitude, this.latitude]))
+          }),
+          new ol.Feature({
+            geometry: new ol.geom.Point(ol.proj.fromLonLat([this.longitude+1, this.latitude+1]))
           })
         ]
       })
     });
     this.map.addLayer(layer);
-  }
 
+    var popup = new ol.Overlay({
+      element: document.getElementById('popup')
+    });
+    this.map.addOverlay(popup);
+    this.map.on('click', function(evt) {
+      var feature = this.forEachFeatureAtPixel(evt.pixel, function (feature, layer) {
+        return feature;
+      });
+      if (feature) {
+        var coord = feature.getGeometry().getCoordinates();
+      }
+      var ol3_sprint_location = coord;
+      popup.setPosition(ol3_sprint_location);
+    });
+  }
 }
