8b109f77f4d1a98ebf4d3021a4f41700d24788d4
xNieuczesana <weronka1999@gmail.com>
2021-06-02 18:13:47 +0200

    API for ecodrive and tracks implemented

--- src/app/ecodrive/ecodrive.component.ts
+++ src/app/ecodrive/ecodrive.component.ts
@@ -1,4 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+
+interface LoginStatus {
+  logged: boolean;
+  rbac: string;
+  nickname: string;
+}
+
+interface ListOfUser {
+  revolutions: number;
+  combustion: number;
+  rate: number;
+  surname: string;
+  name: string;
+  id: number;
+  tracks: number;
+  speed: number;
+}
+
+interface ListOfEco {
+  page: number;
+  listOfUsers: ListOfUser[];
+  pageMax: number;
+}
 
 @Component({
   selector: 'app-ecodrive',
@@ -7,9 +32,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EcodriveComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+  listOfUsers: ListOfEco;
+  page: number;
+  pageMax: number;
+  pageSize = 6;
 
   ngOnInit(): void {
+    this.http.get<LoginStatus>('/API/authorization/status').subscribe(value => {
+      if (!value.logged) {
+        this.router.navigate(['./']);
+      }
+    });
+    this.list(1, '$');
+  }
+
+  public list(page: number, regex: string): void {
+    if (regex === '') {
+      regex = '$';
+    }
+    if (page >= 1) {
+      this.http.get<ListOfEco>('/API/ecoPoints/list/' + page + '/' + this.pageSize + '/' + regex + '/').subscribe(value => {
+        if (page <= value.pageMax) {
+          this.listOfUsers = value;
+          this.page = value.page;
+          this.pageMax = value.pageMax;
+        }
+      });
+    }
   }
 
 }
--- src/app/tracks/tracks.component.ts
+++ src/app/tracks/tracks.component.ts
@@ -1,4 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import {HttpClient} from "@angular/common/http";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Subscription} from "rxjs";
+
+interface LoginStatus {
+  logged: boolean;
+  rbac: string;
+  nickname: string;
+}
+
+interface ListOfTrack {
+  distance: number;
+  start: number;
+  from: string;
+  end: number;
+  id: number;
+  to: string;
+}
+
+interface ListOfTracks {
+  page: number;
+  user: string;
+  pageMax: number;
+  listOfTracks: ListOfTrack[];
+}
 
 @Component({
   selector: 'app-tracks',
@@ -6,10 +32,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tracks.component.scss']
 })
 export class TracksComponent implements OnInit {
+  private routeSub: Subscription;
+  private id:number;
+  dateFromValue: string;
+  dateToValue: string;
+  listOfTracks: ListOfTracks;
+  page: number;
+  pageMax: number;
+  pageSize = 6;
 
-  constructor() { }
+  constructor(private datePipe: DatePipe, private route: ActivatedRoute, private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    this.http.get<LoginStatus>('/API/authorization/status').subscribe(value => {
+      if (!value.logged) {
+        this.router.navigate(['./']);
+      }
+    });
+    this.dateFromValue = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
+    this.dateToValue = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
+    this.list(1);
+  }
+
+  public list(page: number): void {
+    if (page >= 1) {
+      this.routeSub = this.route.params.subscribe(params => {
+        this.id = params['id'];
+        this.http.get<ListOfTracks>('/API/track/list/' + this.id + '/' + page + '/' + this.pageSize + '/' + this.dateFromValue + '/' + this.dateToValue + '/').subscribe(value => {
+          if (page <= value.pageMax) {
+            this.listOfTracks = value;
+            this.page = value.page;
+            this.pageMax = value.pageMax;
+          }
+        });
+      });
+    }
   }
 
 }
