5bee2d5fd8262d393721e5d49894aa4557302b7f
xNieuczesana <weronka1999@gmail.com>
2021-10-26 21:49:44 +0200

    safety for user without API

--- src/app/safety-user/safety-user.component.ts
+++ src/app/safety-user/safety-user.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {PageService} from "../services/page.service";
+import {Router} from "@angular/router";
+import {SafetyService} from "../services/safety.service";
 
 @Component({
   selector: 'app-safety-user',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SafetyUserComponent implements OnInit {
 
-  constructor() { }
+  @Input() popup = false;
+  @Output() popupChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  constructor(private safetyService: SafetyService, private pageService: PageService, private router: Router) { }
 
   ngOnInit(): void {
+    this.pageService.getLoginStatus().subscribe(value => {
+      if (!value.logged) {
+        this.router.navigate(['./']);
+      }
+    });
+  }
+
+  closeWindow() {
+    this.popup = !(this.popup);
+    this.popupChange.emit(this.popup);
   }
 
 }
