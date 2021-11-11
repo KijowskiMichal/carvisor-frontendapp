d523d409cf7b80a8ab15ab7b4ac8074764dfb4a7
xNieuczesana <weronka1999@gmail.com>
2021-11-11 19:45:50 +0100

    add event finished

--- src/app/add-event/add-event.component.ts
+++ src/app/add-event/add-event.component.ts
@@ -1,4 +1,6 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, Renderer2, ViewChild} from '@angular/core';
+import {DatePipe} from "@angular/common";
+import {listNames, MapService} from "../services/map.service";
 
 @Component({
   selector: 'app-add-event',
@@ -9,18 +11,61 @@ export class AddEventComponent implements OnInit {
 
   @Input() popup = false;
   @Output() popupChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @ViewChild('popupTrigger') toggleButton!: ElementRef;
+
+  constructor(public datePipe: DatePipe, private mapService: MapService, private renderer: Renderer2) {
+    this.renderer.listen('window', 'click',(e:Event)=>{
+      if(e.target !== this.toggleButton.nativeElement && this.popupOn){
+        this.disablePopup();
+      }
+    });
+  }
 
   popupOk = false;
   popupFail = false;
+  popupOn = false;
+  names!: listNames[];
+  popupTrigger!: HTMLDivElement;
+  dateFromValue!:string;
+  dateToValue!:string;
+  dateFromTimestamp!: number;
+  dateToTimestamp!: number;
+  deviceID = 0;
 
-  constructor() { }
+  ngAfterViewInit() {
+    this.popupTrigger = this.toggleButton.nativeElement;
+  }
 
   ngOnInit(): void {
+    this.dateFromValue = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
+    this.dateToValue = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
   }
 
   closeWindow() {
     this.popup = !(this.popup);
     this.popupChange.emit(this.popup);
+  }
+
+  showPopup() {
+    this.popupOn = true;
+    this.popupTrigger.classList.add("popupOpen");
+  }
+
+  disablePopup() {
+    this.popupTrigger.classList.remove("popupOpen");
+    this.popupOn = false;
+  }
+
+  chooseOption(value:number, text:string) {
+    this.deviceID = value;
+    this.popupTrigger.innerHTML = text;
+  }
+
+  showDevices() {
+    this.mapService.getListOfDevice('$').subscribe(value => {
+      this.names = value;
+      this.showPopup();
+    });
   }
 
 }
