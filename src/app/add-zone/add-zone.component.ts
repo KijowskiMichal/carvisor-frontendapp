a7c61d1fc46c497fe39de00da34b2c52d8128d29
xNieuczesana <weronka1999@gmail.com>
2021-11-30 18:50:57 +0100

    zones map

--- src/app/add-zone/add-zone.component.ts
+++ src/app/add-zone/add-zone.component.ts
@@ -1,5 +1,7 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {ZoneService} from "../services/zone.service";
+import {listNames, Rate} from "../services/map.service";
+declare var ol: any;
 
 @Component({
   selector: 'app-add-zone',
@@ -8,20 +10,89 @@ import {ZoneService} from "../services/zone.service";
 })
 export class AddZoneComponent implements OnInit {
 
+  private map!: any;
+
   @Input() popup = false;
   @Output() popupChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @ViewChild('mapElementRef', { static: true }) mapElementRef: ElementRef;
 
   popupOk = false;
   popupFail = false;
 
+  latitude: number = 52.460394146699365;
+  longitude: number = 16.917809968543395;
+  rates!: Rate;
+  names!: listNames[];
+  points: Array<any> = [];
+  empList: Array<any> = [];
+  line: Array<any> = [];
+  popupik!: string;
+  popupTrigger!: HTMLDivElement;
+  popupDiv!: HTMLDivElement;
+  maxDate!:string;
+  dateValue!:string;
+  dateTimestamp!: number;
+  radiusValue: string = '1000';
+  circle: any;
+  posY: string;
+  posX: string;
+
   constructor(public zoneService: ZoneService) {}
 
   ngOnInit(): void {
+    this.map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([this.longitude, this.latitude]),
+        zoom: 14
+      })
+    });
+    this.map.setTarget(this.mapElementRef.nativeElement);
+    this.map.on('click', function(evt) {
+      var click = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
+      this.drawCircle(click[0], click[1]);
+      this.posY = click[0];
+      this.posX = click[1];
+    }, this);
+  }
+
+  drawCircle(posY, posX) {
+    this.map.removeLayer(this.circle);
+    var centerLongitudeLatitude = ol.proj.fromLonLat([Number.parseFloat(posY), Number.parseFloat(posX)]);
+    this.circle = new ol.layer.Vector({
+      source: new ol.source.Vector({
+        projection: 'EPSG:4326',
+        features: [new ol.Feature(new ol.geom.Circle(centerLongitudeLatitude, Number.parseInt(this.radiusValue)))]
+      }),
+      style: [
+        new ol.style.Style({
+          stroke: new ol.style.Stroke({
+            color: 'blue',
+            width: 3
+          }),
+          fill: new ol.style.Fill({
+            color: 'rgba(0, 0, 255, 0.1)'
+          })
+        })
+      ]
+    });
+    this.map.addLayer(this.circle);
   }
 
   closeWindow() {
     this.popup = !(this.popup);
     this.popupChange.emit(this.popup);
+    this.posY = null;
+    this.posX = null;
+    this.radiusValue = '1000';
+    this.map.removeLayer(this.circle);
+    this.map.getView().setCenter(ol.proj.fromLonLat([this.longitude, this.latitude]));
+    this.map.getView().setZoom(14);
   }
 
   sendForm(name:HTMLInputElement, pointX:HTMLInputElement, pointY:HTMLInputElement, radius:HTMLInputElement) {
